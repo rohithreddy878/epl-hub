fc8e578bb0271fb63a10b9dca126513f7808bf83
rohithreddy878 <rohithreddy878@gmail.com>
2025-02-28 14:30:38 -0600

    1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from "./components/Header";
+import TitleSection from "./components/dashboard/TitleSection";
+import NewsSection from "./components/dashboard/NewsSection";
+import Teams from "./pages/Teams";
+import Standings from "./pages/Standings";
+import Stats from "./pages/Stats";
+
+const Home = () => (
+    <main className="App-homecontent">
+        <TitleSection />
+        <NewsSection />
+    </main>
+);
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <div className="App">
+                {/* Global Header */}
+                <Header />
+
+                {/* Route Configuration */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/teams" element={<Teams />} />
+                    <Route path="/standings" element={<Standings />} />
+                    <Route path="/stats" element={<Stats />} />
+                </Routes>
+
+                {/* Global Footer */}
+                <footer className="App-footer">
+                    <p>© 2025 My React App</p>
+                </footer>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
